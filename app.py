9f17ca4e1dec8aa215ba46bfc902b7e5c7b466c4
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-07-20 21:38:14 +0530

    guest, hostroute created

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify,Response
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson import json_util
+from bson import ObjectId
+from bson.errors import InvalidId
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+
+from Routes.users import auth_bp
+from Routes.users import user_bp
+
+
+# Create the Flask app
+    # app = Flask(__name__)
+    # CORS(app)
+    # client = MongoClient('mongodb+srv://ashik:ashik@cluster0.t6n9xjt.mongodb.net/?retryWrites=true&w=majority')
+    # db = client['trip_heaven']
+    # collection = db['user']
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    app.mongo_client = MongoClient(app.config['MONGO_URI'])
+    app.db = app.mongo_client['trip_heaven']
+    collection = app.db['user']
+
+    # check if mongoDB connected
+    try:
+        # Access a collection to check the connection
+        count = collection.count_documents({})
+        print(f"MongoDB connected. Collection count: {count}")
+    except ConnectionError as e:
+        print(f"Failed to connect to MongoDB. Error: {str(e)}")
+
+
+    
+    # Register the blueprints with the app
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
+
+    return app
+    
+app = create_app()
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+
